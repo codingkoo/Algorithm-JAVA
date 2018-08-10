87d77002d0e2f87939f1f2d1ede8d17dafd75e7f
codingkoo <codingkoo@gmail.com>
2018-08-10 19:12:42 +0900

    소수를 구하는 가장 기본적인 방법

---
+++ algorithm-Java/src/book/basic/PrimeNumber1.java
@@ -0,0 +1,18 @@
+package book.basic;
+
+public class PrimeNumber1 {
+	public static void main(String args[]) {
+		int max = 50;
+		for(int i = 2; i <= max; i++) {
+			for(int n = 2; n < i; n++) {
+				if(i%n == 0) {
+					break;
+				}
+				
+				if(i == n+1) {
+					System.out.println(i);
+				}
+			}
+		}
+	}
+}
