d9e5afce10605a23c137e16aca4bb349ceb9886e
codingkoo <codingkoo@gmail.com>
2018-08-10 20:08:06 +0900

    소수 찾기 무식한 알고리즘

--- algorithm-Java/src/book/basic/PrimeNumber1.java
+++ algorithm-Java/src/book/basic/PrimeNumber1.java
@@ -2,9 +2,10 @@ package book.basic;
 
 public class PrimeNumber1 {
 	public static void main(String args[]) {
-		int max = 50;
+		int max = 103;
 		for(int i = 2; i <= max; i++) {
 			for(int n = 2; n < i; n++) {
+				
 				if(i%n == 0) {
 					break;
 				}
