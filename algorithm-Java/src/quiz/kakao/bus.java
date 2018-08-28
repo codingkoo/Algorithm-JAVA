a573dcc4ec67045e06a622c2010f175fc5648901
Codingkoo <codingkoo@gmail.com>
2018-08-28 09:53:27 +0900

    카카오 4번 버스정거장 문제풀이(중도포기)

---
+++ algorithm-Java/src/quiz/kakao/bus.java
@@ -0,0 +1,29 @@
+package quiz.kakao;
+
+
+import java.sql.Time;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class bus {
+
+    public static void main(String[] args) {
+        String[] timeTable = {"08:00", "08:01", "08:02", "08:03"};
+        System.out.println(solution(1, 1, 5, timeTable));
+        
+    }
+    
+    
+    public static String solution(int n, int t, int m, String[] timetable) {
+        
+        
+        LinkedList<String> busTable = new LinkedList<>();
+
+        
+        
+        
+        String answer = "";
+        return answer;
+    }
+
+}
