2f825522e66d8ea2f204519d4861ff727d296c65
Codingkoo <codingkoo@gmail.com>
2018-09-16 18:22:19 +0900

    BOJ 11723 집합 - 비트마스크 문제 풀이 완료

---
+++ algorithm-Java/src/boj/codePlus/BitCalculate.java
@@ -0,0 +1,41 @@
+package boj.codePlus;
+
+import java.util.Scanner;
+
+public class BitCalculate {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int bitmask = 0;
+        int TestCase = scan.nextInt();
+        for(int i = 0; i < TestCase; i++) {
+            String command = scan.next();
+            if(command.equals("add")) {
+                int number = scan.nextInt();
+                bitmask = (bitmask | (1 << number));
+            }else if(command.equals("remove")) {
+                int number = scan.nextInt();
+                bitmask = (bitmask & ~(1 << number));
+            }else if(command.equals("check")) {
+                int number = scan.nextInt();
+                int res = (bitmask & (1 << number));
+                if(res != 0)
+                    System.out.println(1);
+                else
+                    System.out.println(0);
+            } else if(command.equals("toggle")) {
+                int number = scan.nextInt();
+                bitmask = (bitmask ^ (1<<number));
+            } else if(command.equals("all")) {
+                bitmask = (1<<20) -1;
+            } else if(command.equals("empty")){
+                bitmask = 0;
+            }
+        }
+        
+        scan.close();
+        
+        
+    }
+
+}
