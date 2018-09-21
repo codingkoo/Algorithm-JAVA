b171179be25cb056cf70283a9575dc2b8c603dc9
Codingkoo <codingkoo@gmail.com>
2018-09-22 04:36:45 +0900

    for문을 활용한 순열 연습

--- algorithm-Java/src/book/basic/PermutationNumberFor.java
+++ algorithm-Java/src/book/basic/PermutationNumberFor.java
@@ -6,7 +6,7 @@ public class PermutationNumberFor {
     
     // Using 1, 2, 3, 4
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4};
+        int[] numbers = {1, 2, 3, 4 };
         
         for(int i = 0; i < numbers.length; i++) {
             for(int j = 0; j < numbers.length; j++) {
