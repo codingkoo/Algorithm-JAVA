ecbe610b497145fd66c5bdf4ed42a31ff1758c29
Codingkoo <codingkoo@gmail.com>
2018-09-22 04:35:46 +0900

    for문을 활용한 숫자 순열 연습

--- algorithm-Java/src/book/basic/PermutationNumberFor.java
+++ algorithm-Java/src/book/basic/PermutationNumberFor.java
@@ -2,29 +2,25 @@ package book.basic;
 
 import java.util.LinkedList;
 
-public class PermutationFor {
+public class PermutationNumberFor {
     
     // Using 1, 2, 3, 4
     public static void main(String[] args) {
-        LinkedList<Integer> numbers = new LinkedList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
+        int[] numbers = {1, 2, 3, 4};
         
-        for(int i = 0; i < numbers.size(); i++) {
-            for(int j = 0; j < numbers.size(); j++) {
-                if(numbers.get(j) != numbers.get(i)) {
-                    for(int k = 0; k < numbers.size(); k++) {
-                        if(numbers.get(k) != numbers.get(i) & numbers.get(k) != numbers.get(j)) {
-                            for(int l = 0; l < numbers.size(); l++) {
-                                if(numbers.get(l) != numbers.get(i) & numbers.get(l) != numbers.get(j) 
-                                        & numbers.get(l) != numbers.get(k)) {
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = 0; j < numbers.length; j++) {
+                if(numbers[j] != numbers[i]) {
+                    for(int k = 0; k < numbers.length; k++) {
+                        if(numbers[k] != numbers[i] & numbers[k] != numbers[j]) {
+                            for(int l = 0; l < numbers.length; l++) {
+                                if(numbers[l] != numbers[i] & numbers[l] != numbers[j] 
+                                        & numbers[l] != numbers[k]) {
                                     StringBuilder sb = new StringBuilder();
-                                    sb.append(numbers.get(i));
-                                    sb.append(numbers.get(j));
-                                    sb.append(numbers.get(k));
-                                    sb.append(numbers.get(l));
+                                    sb.append(numbers[i]);
+                                    sb.append(numbers[j]);
+                                    sb.append(numbers[k]);
+                                    sb.append(numbers[l]);
                                     System.out.println(sb);
                                 }
                             }
